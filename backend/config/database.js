1e569bb20badc73d09a059071df8d20fb8b080fa
Piotr Mikolajczuk <piotr@mikolajczuk.in>
2024-02-13 12:20:09 +0100

    Changed backend logic

--- backend/config/database.js
+++ backend/config/database.js
@@ -99,7 +99,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "piotr"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "ljzZ666HZP"),
+      password: env("DATABASE_PASSWORD", "test"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       // ssl: {
       //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
